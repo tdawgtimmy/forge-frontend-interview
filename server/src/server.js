37a135c31db0aa992eefe7161527bb27701806a3
Andrew Bantug <andrew.bantug@honeywell.com>
2020-09-08 10:46:32 -0400

    api update

--- server/src/server.js
+++ server/src/server.js
@@ -9,21 +9,25 @@ app.get('/', function (req, res) {
     res.send('Welcome to Forge Platform Interview');
 });
 
-app.get('/getNotifications', function (req, res) {
+app.get('/notifications', function (req, res) {
     res.send(notifications);
 });
 
-app.put('/addNotification', function (req, res) {
+app.post('/notifications', function (req, res) {
     const newNotification = req.body;
     notifications.push(newNotification);
-    res.send(notifications);
+    res.status(201).send(newNotification);
 });
 
-app.delete('/removeNotification/:id', function (req, res) {
+app.delete('/notifications/:id', function (req, res) {
     const id = req.params.id;
-    const index = notifications.findIndex(x => x.id);
-    notifications.splice(index, 1);
-    res.send(notifications);
+    const index = notifications.findIndex(notification => notification.id === parseInt(id));
+    if (index >= 0) {
+        notifications.splice(index, 1);
+        res.status(204).send()
+    } else {
+        res.status(404).send()
+    }
 });
 
 app.listen(8080, () => console.log('server started at port 8080'));
